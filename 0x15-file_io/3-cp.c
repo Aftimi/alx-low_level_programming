2fde8046d0bd9eab22b99a7418b5f80b16c456ae
Aftimi <meriem.aftimi1997@gmail.com>
2023-05-09 15:16:38 -0700

    t

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,7 +40,7 @@ file_from = open(argv[1], O_RDONLY);
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 error_file(file_from, file_to, argv);
 nchars = 1024;
-while (nchars == 1024)
+hile (nchars == 1024)
 {
 nchars = read(file_from, buf, 1024);
 if (nchars == -1)
